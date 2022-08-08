c28f5947cb086ea4bd4249e8afef6e21504f36c5
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-08-08 21:17:27 +0300

    hw1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// {
-// 	/*
-// 	 * Prevents the lightbox from closing when clicking its background.
-// 	 */
-// 	closable: true,
-// 	/*
-// 	 * One or more space separated classes to be added to the basicLightbox element.
-// 	 */
-// 	className: '',
-// 	/*
-// 	 * Function that gets executed before the lightbox will be shown.
-// 	 * Returning false will prevent the lightbox from showing.
-// 	 */
-	// onShow: (instance) => {},
-// 	/*
-// 	 * Function that gets executed before the lightbox closes.
-// 	 * Returning false will prevent the lightbox from closing.
-// 	 */
-// 	onClose: (instance) => {}
-// };
-
-console.log(galleryItems);
 const refs = {
   boxGallery: document.querySelector(".gallery"),
 };
@@ -46,28 +24,26 @@ const addGalleryItem = () => {
 };
 refs.boxGallery.insertAdjacentHTML("beforeend", addGalleryItem());
 
-
-const onOpenImg = (e) => {
+refs.boxGallery.addEventListener("click", onOpenImg);
+function onOpenImg(e) {
   e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
-    return
+  if (e.target.nodeName !== "IMG") {
+    return;
   }
 
   const largeImg = e.target.dataset.source;
   // console.log(largeImg);
 
   const instance = basicLightbox.create(`
-  <img src="${largeImg}"  >
+  <img src="${largeImg}">
 `);
-instance.show();
-
-  // console.dir(e.target.dataset.source );
-};
-refs.boxGallery.addEventListener("click", onOpenImg);
-
-// const basicLightbox = require("basiclightbox");
-// import * as basicLightbox from "basiclightbox";
-
-
-
-console.log(refs.boxGallery);
+  instance.show();
+  window.addEventListener("keydown", onKey);
+  function onKey(e) {
+    if (e.key === "Escape") {
+      instance.close();
+      console.dir(e.key);
+      window.removeEventListener("keydown", onKey);
+    }
+  }
+}

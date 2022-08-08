1c3eb10f96226a5b4c70fcfaa62bc27f00377e1a
Alexandr Gorshkoder <88847067+sanlander@users.noreply.github.com>
2022-08-08 19:38:35 +0300

    +gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,38 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-{
-	/*
-	 * Prevents the lightbox from closing when clicking its background.
-	 */
-	closable: true,
-	/*
-	 * One or more space separated classes to be added to the basicLightbox element.
-	 */
-	className: '',
-	/*
-	 * Function that gets executed before the lightbox will be shown.
-	 * Returning false will prevent the lightbox from showing.
-	 */
-	onShow: (instance) => {},
-	/*
-	 * Function that gets executed before the lightbox closes.
-	 * Returning false will prevent the lightbox from closing.
-	 */
-	onClose: (instance) => {}
-}
+// {
+// 	/*
+// 	 * Prevents the lightbox from closing when clicking its background.
+// 	 */
+// 	closable: true,
+// 	/*
+// 	 * One or more space separated classes to be added to the basicLightbox element.
+// 	 */
+// 	className: '',
+// 	/*
+// 	 * Function that gets executed before the lightbox will be shown.
+// 	 * Returning false will prevent the lightbox from showing.
+// 	 */
+	// onShow: (instance) => {},
+// 	/*
+// 	 * Function that gets executed before the lightbox closes.
+// 	 * Returning false will prevent the lightbox from closing.
+// 	 */
+// 	onClose: (instance) => {}
+// };
 
 console.log(galleryItems);
 const refs = {
-  boxGallery: document.querySelector('.gallery'),
+  boxGallery: document.querySelector(".gallery"),
 };
-
-const onOpenImg = e => {
-  e.preventDefault();
-
-  console.log(e.target.dataset.source);
-};
-refs.boxGallery.addEventListener('click', onOpenImg);
 
 const addGalleryItem = () => {
   return galleryItems
@@ -49,14 +42,32 @@ const addGalleryItem = () => {
       </a>
     </div>`;
     })
-    .join('');
+    .join("");
 };
-refs.boxGallery.insertAdjacentHTML('beforeend', addGalleryItem());
+refs.boxGallery.insertAdjacentHTML("beforeend", addGalleryItem());
 
-const instance = basicLightbox.create(`
-    <img src="">
+
+const onOpenImg = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return
+  }
+
+  const largeImg = e.target.dataset.source;
+  // console.log(largeImg);
+
+  const instance = basicLightbox.create(`
+  <img src="${largeImg}"  >
 `);
-
 instance.show();
+
+  // console.dir(e.target.dataset.source );
+};
+refs.boxGallery.addEventListener("click", onOpenImg);
+
+// const basicLightbox = require("basiclightbox");
+// import * as basicLightbox from "basiclightbox";
+
+
 
 console.log(refs.boxGallery);
